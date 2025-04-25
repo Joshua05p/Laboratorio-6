819a78ee21b1229a488bf4467bb62bd390576e85
Joshua05p <per23395@uvg.edu.gt>
2025-04-25 14:13:08 -0600

    Create INTaCHAR.h

---
+++ INTaCHAR.h
@@ -0,0 +1,14 @@
+#ifndef INTACHAR_H_
+#define INTACHAR_H_
+#include <avr/io.h>
+
+
+void setup();
+void iniciar_USART();
+void enviar(char data);
+void cadena();
+void iniciar_adc();
+void iniciar_conversion();
+
+
+#endif /* INTACHAR_H_ */
